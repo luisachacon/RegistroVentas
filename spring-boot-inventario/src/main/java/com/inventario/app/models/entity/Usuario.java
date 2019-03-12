65f0c5dfb65ae68b51df554aabad34d205ab27af
Luisa Chacón <lchacon565@unab.edu.co>
2019-03-11 21:09:30 -0500

    sql

--- spring-boot-inventario/src/main/java/com/inventario/app/models/entity/Usuario.java
+++ spring-boot-inventario/src/main/java/com/inventario/app/models/entity/Usuario.java
@@ -1,5 +1,42 @@
 package com.inventario.app.models.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private Long id;
+	
+	@Column(name = "contrasenia")
+	private String contrasenia;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	
+	
 }
